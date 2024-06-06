3239170e368c1f7dea4c5ec1f5d8e6e627752fbc
Dinda Putri <152158224+dindapoetri@users.noreply.github.com>
2024-06-06 18:47:33 +0700

    Update PPCD_UAS.py

--- PPCD_UAS.py
+++ PPCD_UAS.py
@@ -1,10 +1,10 @@
 ## Meng-impor modul yang akan digunakan untuk ##
-import cv2 # Modul untuk mengolah citra digital 
+# import cv2 # Modul untuk mengolah citra digital 
 import numpy as np
 import streamlit as st
 # import PySimpleGUI as sg
 import tempfile
 import os
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
 st.title('Uber pickups in NYC')
