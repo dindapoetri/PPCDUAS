1bacb5b39f557651915695fc6a41c3e57de455d1
Dinda Putri <152158224+dindapoetri@users.noreply.github.com>
2024-06-06 17:30:30 +0700

    Update PPCD_UAS.py

--- PPCD_UAS.py
+++ PPCD_UAS.py
@@ -2,7 +2,7 @@
 import cv2 # Modul untuk mengolah citra digital 
 import numpy as np
 import streamlit as st
-import PySimpleGUI as sg
+# import PySimpleGUI as sg
 import tempfile
 import os
 from matplotlib import pyplot as plt
